f2bd5bcfe4cfa185ed4d90e3f642f43de43446c8
Alisha Bohara <alishabohara369@gmail.com>
2025-11-26 13:02:49 +0545

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import Footer from "./components/Footer.jsx";
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,8 +28,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+        <Footer/>
     </>
+  
+    
   )
 }
 
 export default App
+
+
+
