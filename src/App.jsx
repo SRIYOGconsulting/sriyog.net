794ef57ac591b83ef9866a973f4bb350d167879f
Sneha-Neupanee <neupanesneha648@gmail.com>
2025-11-26 13:36:46 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Outlet } from "react-router-dom";
 
 export default function App() {
@@ -8,31 +7,3 @@ export default function App() {
     </div>
   );
 }
-=======
-
-import RoadBlock from './Components/RoadBlock'
-import './App.css'
-
-import Footer from "./components/Footer.jsx";
-function App() {
-  const onClose = ()=>{
-      const closeDiv = document.querySelector(".close") as HTMLElement | null;
-
-      if (closeDiv) {
-        closeDiv.style.display = "none";
-      }
-  }
-  return (
-    <>
-        <RoadBlock onClose={onClose}/>
-    </>
-  
-    
-  )
-}
-
-export default App
-
-
-
->>>>>>> 84eced7181fd050282e99901891da9904db296fb
