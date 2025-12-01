8d734904e61976cdaf3024428bd0cab71a4a1fd0
smritibhusal12 <smritibhusal467@gmail.com>
2025-12-01 13:56:04 +0545

    Update icons

--- src/Components/MobileDrawer.jsx
+++ src/Components/MobileDrawer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // Reusable Social Icon Component
-const SocialIcon = ({ href, label, children }) => (
+const SocialIcon = ({ href, label, icon }) => (
   <a 
     href={href}
     target="_blank"
@@ -10,9 +10,12 @@ const SocialIcon = ({ href, label, children }) => (
     className="flex items-center justify-center w-9 h-9 rounded-full bg-gray-100 hover:bg-gray-200 transition-colors"
     aria-label={label}
   >
-    <svg className="w-4.5 h-4.5" fill="currentColor" viewBox="0 0 24 24">
-      {children}
-    </svg>
+    <img 
+      src={`/icons/${icon}`} 
+      alt={label}
+      className="w-5 h-5"
+      aria-hidden="true"
+    />
   </a>
 );
 
@@ -55,22 +58,37 @@ const MobileDrawer = () => {
     {
       href: 'https://www.facebook.com/',
       label: 'Facebook',
-      path: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.563V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z'
+      icon: 'facebook.svg'
     },
     {
       href: 'https://www.youtube.com/',
       label: 'YouTube',
-      path: 'M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z'
+      icon: 'youtube.svg'
     },
     {
-      href: 'https://www.instagram.com/',
-      label: 'Instagram',
-      path: 'M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.7-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z'
-    },
-    {
-      href: 'https://www.linkedin.com/company/',
+      href: 'https://www.linkedin.com/',
       label: 'LinkedIn',
-      path: 'M20.5 2h-17A1.5 1.5 0 0 0 2 3.5v17A1.5 1.5 0 0 0 3.5 22h17a1.5 1.5 0 0 0 1.5-1.5v-17A1.5 1.5 0 0 0 20.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 1 1 8.3 6.5a1.78 1.78 0 0 1-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0 0 13 14.19V19h-3v-9h2.9v1.3a3.11 3.11 0 0 1 2.7-1.4c1.55 0 3.4.9 3.4 4.2z'
+      icon: 'linkedin.svg'
+    },
+    {
+      href: 'https://t.me/',
+      label: 'Telegram',
+      icon: 'telegram.svg'
+    },
+    {
+      href: 'https://wa.me/',
+      label: 'WhatsApp',
+      icon: 'whatsapp.svg'
+    },
+    {
+      href: 'https://www.messenger.com/',
+      label: 'Messenger',
+      icon: 'messenger.svg'
+    },
+    {
+      href: 'https://www.viber.com/',
+      label: 'Viber',
+      icon: 'viber.svg'
     }
   ];
 
@@ -193,9 +211,12 @@ const MobileDrawer = () => {
               </p>
               <div className="flex flex-wrap justify-center gap-3 px-2">
                 {socialLinks.map((social) => (
-                  <SocialIcon key={social.href} href={social.href} label={social.label}>
-                    <path d={social.path} />
-                  </SocialIcon>
+                  <SocialIcon 
+                    key={social.href} 
+                    href={social.href} 
+                    label={social.label}
+                    icon={social.icon}
+                  />
                 ))}
               </div>
             </div>
